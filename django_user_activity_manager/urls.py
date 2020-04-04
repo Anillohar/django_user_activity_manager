bf68b838599cfdb1cdbf79cd957fa6d2e4af27fe
anil <anillohar03@gmail.com>
2020-04-04 22:56:41 +0530

    changed login, logout url

--- django_user_activity_manager/urls.py
+++ django_user_activity_manager/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from user_management.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('admin/login/', LoginView.as_view(), name='login'),
-    path('admin/logout/', LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('', admin.site.urls),
     path('api/v1/', include('api.urls'))
 ]
