39e952c967951253c5f0939fad38b7c2ba7afad1
anil <anillohar03@gmail.com>
2020-04-04 22:30:43 +0530

    changes in admin url

--- django_user_activity_manager/urls.py
+++ django_user_activity_manager/urls.py
@@ -5,6 +5,6 @@ from user_management.views import LoginView, LogoutView
 urlpatterns = [
     path('admin/login/', LoginView.as_view(), name='login'),
     path('admin/logout/', LogoutView.as_view(), name='logout'),
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/v1/', include('api.urls'))
 ]
